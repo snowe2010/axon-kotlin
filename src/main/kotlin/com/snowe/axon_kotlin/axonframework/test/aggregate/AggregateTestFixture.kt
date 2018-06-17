32a0513404036da0fd62a9686e69aaf20b5b2c93
DRAGONBORN\Tyler <tylerbthrailkill@gmail.com>
2018-06-17 00:08:42 -0600

    Include defaulted metadata in whenever inline function

--- src/main/kotlin/com/snowe/axon_kotlin/axonframework/test/aggregate/AggregateTestFixture.kt
+++ src/main/kotlin/com/snowe/axon_kotlin/axonframework/test/aggregate/AggregateTestFixture.kt
@@ -2,6 +2,7 @@ package com.snowe.axon_kotlin.axonframework.test.aggregate
 
 import org.axonframework.commandhandling.CommandMessage
 import org.axonframework.messaging.MessageHandler
+import org.axonframework.messaging.MetaData
 import org.axonframework.test.aggregate.AggregateTestFixture
 import org.axonframework.test.aggregate.ResultValidator
 import org.axonframework.test.aggregate.TestExecutor
@@ -11,7 +12,7 @@ import org.hamcrest.Matchers.hasProperty
 inline fun <reified T> AggregateTestFixture() = org.axonframework.test.aggregate.AggregateTestFixture(T::class.java)
 inline fun <reified T> AggregateTestFixture<*>.registerCommandHandler(commandHandler: MessageHandler<CommandMessage<*>>) = this.registerCommandHandler(T::class.java, commandHandler)
 inline fun <reified T> AggregateTestFixture<*>.registerIgnoredField(fieldName: String) = this.registerIgnoredField(T::class.java, fieldName)
-fun TestExecutor.whenever(command: Any): ResultValidator = this.`when`(command)
+fun TestExecutor.whenever(command: Any, metaData: Map<String, Any> = MetaData.emptyInstance()): ResultValidator = this.`when`(command, metaData)
 
 /**
  * Expect Exception with a reified class and a provided Exception message
