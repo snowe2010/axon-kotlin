6f71386a7e40f1a51a6122e569bf38e533c5e0e6
Tyler Thrailkill <tylerbthrailkill@gmail.com>
2018-07-01 16:05:55 -0600

    Publish of Github pages from Gradle.

--- axon-kotlin-test/src/main/kotlin/com/tylerthrailkill/axon_kotlin/axonframework/test/exceptions/AxonKotlinTestException.kt
+++
@@ -1,3 +0,0 @@
-package com.tylerthrailkill.axon_kotlin.axonframework.test.exceptions
-
-class AxonKotlinTestException(override val message: String?) : RuntimeException()
--- axon-kotlin-test/src/test/kotlin/com/tylerthrailkill/axon_kotlin/axonframework/test/HelperMethods.kt
+++
@@ -1,6 +0,0 @@
-package com.tylerthrailkill.axon_kotlin.axonframework.test
-
-import java.util.*
-
-fun uuid() = UUID.randomUUID()
-fun uuidString() = uuid().toString()
